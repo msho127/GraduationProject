b33764def45c5ceead9fa9a1e22b74241912de63
msho127 <mshotaro1227@gmail.com>
2025-02-03 10:49:15 +0900

    firebase

--- teamproject/src/lib/firebase.ts
+++ teamproject/src/lib/firebase.ts
@@ -3,15 +3,17 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_API,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER,
+  appId: process.env.NEXT_PUBLIC_APP,
 };
 
 const app = initializeApp(firebaseConfig);
+
+
 const db = getFirestore(app);
 export const auth = getAuth(app);
 
